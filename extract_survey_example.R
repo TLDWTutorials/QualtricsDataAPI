f53983a773801ef42dc7aefe46dd6c8ac30c2764
TLDWTutorials <137006032+TLDWTutorials@users.noreply.github.com>
2025-03-03 20:35:21 -0500

    Update extract_survey_example.R

--- extract_survey_example.R
+++ extract_survey_example.R
@@ -1,7 +1,7 @@
 library(qualtRics)
 
-#Enter API info: API Key and base_url
-qualtrics_api_credentials(api_key = "XnMwsXVb87TTGikMRGaaEEHNt5tJbr2vdHRTrYCi", 
+#Enter API info: API Key  and base_url
+qualtrics_api_credentials(api_key = "YOURAPIKEYHERE", 
                           base_url = "ca1.qualtrics.com/")
 
 #This fetches a list of all of your surveys (you can print this - optional)
@@ -11,7 +11,7 @@ surveys <- all_surveys()
 Sys.setenv(TZ = "GMT")
 
 #Select survey to download
-mysurvey <- fetch_survey(surveyID = "SV_1zvPT4k7DJ3tGvQ")
+mysurvey <- fetch_survey(surveyID = "SURVEYIDHERE") #I left the SurveyID blank fo
 
 # Save responses to a CSV file
 write.csv(mysurvey, file = "anonymous_survey.csv", row.names = FALSE)
